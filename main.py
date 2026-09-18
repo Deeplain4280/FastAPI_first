fe872611a288d7e3cba3167e44e31ed4ac113711
Deeplain4280 <17584103548@163.com>
2026-09-18 17:13:38 +0800

    学习orm

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-from fastapi import FastAPI,Path,Query,HTTPException
+from datetime import datetime
+from fastapi import FastAPI,Path,Query,HTTPException,Depends
 from pydantic import BaseModel,Field
 from fastapi.responses import HTMLResponse, FileResponse
+from sqlalchemy import DateTime, String, Float, Integer
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
+from sqlalchemy import func
 #创建fastapi实例
 app = FastAPI()
 
+ASYNC_DATABASE_URL = "mysql+aiomysql://root:200527yp@localhost:3306/fastapi_first?charset=utf8"
+async_engine = create_async_engine(ASYNC_DATABASE_URL,
+                                   echo = True,
+                                   pool_size = 10,
+                                   max_overflow = 20)
 
+class Base(DeclarativeBase):
+    create_time : Mapped[datetime] = mapped_column(DateTime, insert_default=func.now(),default=func.now(), comment="创建时间")
+    update_time : Mapped[datetime] = mapped_column(DateTime, insert_default=func.now(), default=func.now(), onupdate=func.now(), comment="更新时间")
+class Book(Base):
+    __tablename__ = "books"
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, comment="编号")
+    bookname: Mapped[str] = mapped_column(String(255), comment="书名")
+    author : Mapped[str] = mapped_column(String(255), comment="作者")
+    price : Mapped[float] = mapped_column(Float, comment="价格")
+    publisher : Mapped[str] = mapped_column(String(255), comment="出版社")
+
+class User(Base):
+    __tablename__ = "users"
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, comment="编号")
+    username : Mapped[str] = mapped_column(String(255))
+    password : Mapped[str] = mapped_column(String(255))
+
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.on_event("startup")
+async def startup_event():
+    await create_tables()
+
+
+@app.middleware("http")
+async def middleware1(request, call_next):
+    response = await call_next(request)
+    return response
+
+
+@app.middleware("http")
+async def middleware2(request, call_next):
+    response = await call_next(request)
+    return response
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -42,10 +88,13 @@ async def mews(id: int = Path(..., gt=0, lt=101, description="分类id在1到100
 async def news(name: str = Path(..., min_length=2, max_length=10)):
     return {"msg": f"新闻类别为{name}"}
 
-@app.get("/news/news_list")
-async def get_news_list(skip: int = Query(0, lt=100, description="跳过的记录数"),
-                        limit: int = Query(10, description="返回的记录数")):
+async def common_parameters(skip: int = Query(0,ge=0),
+                            limit: int = Query(10, le=60)):
     return {"skip": skip, "limit": limit}
+
+@app.get("/news/news_list")
+async def get_news_list(common = Depends(common_parameters)):
+    return common
 
 @app.get("/book/search")
 async def book_search(catgeory: str = Query("Python开发", min_length=5, max_length=255),
@@ -60,14 +109,14 @@ class User(BaseModel):
 async def register(user: User):
     return user
 
-class Book(BaseModel):
+class Books(BaseModel):
     name: str = Field(..., min_length=2, max_length=20)
     author: str = Field(min_length=2, max_length=10)
     publisher: str = Field(default="黑马出版社")
     price: float = Field(..., gt=0)
 
 @app.post("/addBook")
-async def add_book(book: Book):
+async def add_book(book: Books):
     return book
 
 @app.get("/html", response_class=HTMLResponse)
